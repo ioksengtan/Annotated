f94d2d4086f865e43a325881f82471688697308f
pedroid <ntuyusheng@gmail.com>
2020-01-09 20:38:10 +0800

    20200109

---
+++ website/js/website.js
@@ -0,0 +1,36 @@
+$(document).ready(function(e) {
+	directory = {
+		"home":{
+
+		},
+		"website":{
+			"Stratechery":{
+				0:{
+					"title":"The 2019 Stratechery Year in Review",
+					"link":"https://stratechery.com/2019/the-2019-stratechery-year-in-review/"
+
+				}
+			}
+		},
+		"podcast":{
+
+		}
+	}
+
+	curr_dir = {}
+	curr_dir.type = "website"
+	curr_dir.source = "Stratechery"
+	curr_dir.id = 0
+
+	var display_dir = function(curr_dir){
+		console.log(curr_dir)
+	}
+	var editor = CodeMirror.fromTextArea(document.getElementById("text-input"), {
+	        lineNumbers: true,
+			lineWrapping: true,
+			//readOnly: true
+	    }
+	);
+	editor.setSize("100%","100%");
+}
+)
